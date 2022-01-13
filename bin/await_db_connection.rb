daf7a9a31fd9333066e8619acd114f124fb22f39
tko_t <tkot.bom@gmail.com>
2022-01-13 15:49:06 +0900

    yaml -> psych

--- bin/await_db_connection.rb
+++ bin/await_db_connection.rb
@@ -1,16 +1,18 @@
 require 'active_record'
-require 'yaml'
 require 'erb'
 
 # mysqldが起動されるまでジッと待つ
 config_database =
-  YAML.load(
+  # ruby 3.1系変更による Psych <~ 4.0 対応
+  # https://secret-garden.hatenablog.com/entry/2021/05/23/200803
+  # この辺を参考にした
+  Psych.safe_load(
     ERB.new(
       File.read(
         File.join(__dir__, '../', 'config', 'database.yml')
       ) # raw config/database.yml
-    ).result # parse ERB
-  ) # to_hash
+    ).result, # parse ERB
+    aliases: true) # to_hash
 
 development = config_database.dig('development')
 
